d8b6eb51601bd16b287bf3ea840c2c870ecef473
tjy <1585382647@qq.com>
2023-01-14 22:46:06 +0800

    [ADD] Finish NavBar

---
+++ src/views/userList.vue
@@ -0,0 +1,19 @@
+<template>
+  <ContentBase>首页</ContentBase>
+</template>
+
+<script>
+import ContentBase from '../components/ContentBase';
+
+
+export default {
+  name: 'userList',
+  components: {
+    ContentBase,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
